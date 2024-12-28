1f41431daa995c096f94fbca37cb3b28a0f5d35d
AmirH-Hemati <amirhosseinhemat9@gmail.com>
2024-12-28 12:32:17 +0330

    fixed box to change filter actived

--- src/ui/FilterButton.jsx
+++ src/ui/FilterButton.jsx
@@ -7,7 +7,7 @@ function FilterButton({ text, setFilter, filter }) {
       onClick={() => setFilter(text)}
       className={`${
         filter == text && "bg-[#00582f] text-[#fff]"
-      } bg-blue-500  font-montserrat font-semibold     text-[#00582f] flex items-center justify-center space-x-2  shadow-lg rounded-xl w-32 p-2  `}
+      } bg-[#FBFBFB]  font-montserrat font-semibold     text-[#00582f] flex items-center justify-center space-x-2  shadow-lg rounded-xl w-32 p-2  `}
     >
       <span>{text}</span>
       <Coffee size="26" color={filter === text ? "#fff" : "#00582f"} />
