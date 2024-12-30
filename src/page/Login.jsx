cd7702e7bb288c7bbf40b7ec7317d241e89dceeb
AmirH-Hemati <amirhosseinhemat9@gmail.com>
2024-12-30 22:50:13 +0330

    create login page

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -1,5 +1,27 @@
 function Login() {
-  return <div>login</div>;
+  return (
+    <div className="font-montserrat bg-[#FBFBFB] w-full h-full flex flex-col  justify-center items-center">
+      <form className="flex flex-col gap-2 bg-white shadow-md  rounded-md p-4 justify-evenly md:w-72 aspect-square">
+        <input
+          type="text"
+          placeholder="Username"
+          className="p-3 rounded-md bg-transparent border-gray-300 border border-1 "
+        />
+
+        <input
+          type="password"
+          placeholder="password"
+          className="p-3 rounded-md bg-transparent border-gray-300 border border-1"
+        />
+        <button
+          className="bg-[#00512c] text-white text-sm font-semibold p-3 rounded-md"
+          type="submit"
+        >
+          Login
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
