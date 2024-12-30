207eab9d140d71aa5b10d09c0078b99170f16d5a
AmirH-Hemati <amirhosseinhemat9@gmail.com>
2024-12-30 20:12:53 +0330

    Fixing the image not showing

--- src/ui/Loading.jsx
+++ src/ui/Loading.jsx
@@ -2,7 +2,7 @@ function Loading() {
   return (
     <div
       className="fixed top-0 left-0 w-full h-full bg-cover bg-center bg-no-repeat flex justify-center items-center"
-      style={{ backgroundImage: `url("public/background.png")` }}
+      style={{ backgroundImage: `url("/public/background.png")` }}
     >
       <div className="lds-facebook">
         <div></div>
